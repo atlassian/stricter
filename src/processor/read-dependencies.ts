2a6d0728bfef318beebc7b5ad7d673ddb067d743
Alexey Shpakov <li0liq@gmail.com>
2017-12-12 20:11:26 +1100

    Add support for require and reexport when calculating imports

--- src/processor/read-dependencies.ts
+++ src/processor/read-dependencies.ts
@@ -33,14 +33,22 @@ const getImports = (ast: any) => {
 
     traverse(ast, {
         enter(path: any) {
-            if (path.node.type === 'ImportDeclaration') {
-                staticImports.push(path.node.source.value);
-            } else if (
+            const isStaticImport =
+                path.node.type === 'ImportDeclaration' ||
+                ((path.node.type === 'ExportNamedDeclaration' ||
+                    path.node.type === 'ExportAllDeclaration') &&
+                    path.node.source);
+            const isDynamicImport =
                 path.node.type === 'CallExpression' &&
-                path.node.callee.type === 'Import' &&
+                (path.node.callee.type === 'Import' ||
+                    (path.node.callee.type === 'Identifier' &&
+                        path.node.callee.name === 'require')) &&
                 path.node.arguments &&
-                path.node.arguments.length > 0
-            ) {
+                path.node.arguments.length > 0;
+
+            if (isStaticImport) {
+                staticImports.push(path.node.source.value);
+            } else if (isDynamicImport) {
                 dynamicImports.push(path.node.arguments[0].value);
             }
         },
