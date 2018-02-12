7d8ae5d29ff09166e14cf2498b09c52c8325c2ea
Alexey Shpakov <li0liq@gmail.com>
2018-02-13 08:17:17 +1100

    Improved debug measurements

--- src/debug.ts
+++ src/debug.ts
@@ -1,3 +1,18 @@
 import * as debug from 'debug';
 
-export default debug('stricter');
+const debugWriter = debug('stricter');
+
+export const measure = <T>(mark: string, fn: () => T): T => {
+    if (!debug.enabled) {
+        return fn();
+    }
+
+    const now = Date.now();
+    debugWriter(`⌛ ${mark}`);
+    const result = fn();
+    debugWriter(`✔️ ${mark} (${Date.now() - now}ms)`);
+
+    return result;
+};
+
+export default debugWriter;
--- src/stricter.ts
+++ src/stricter.ts
@@ -4,70 +4,68 @@ import { applyProjectRules, readFilesData } from './processor';
 import { consoleLogger, mochaLogger, compactProjectLogs, getErrorCount } from './logger';
 import { listFiles } from './utils';
 import { StricterArguments, Reporter } from './types';
-import debug from './debug';
+import debug, { measure } from './debug';
 
 export default ({
     silent = false,
     reporter = Reporter.CONSOLE,
     configPath,
 }: StricterArguments): number => {
-    const now = Date.now();
+    const result = measure('Total', () => {
+        debug({
+            silent,
+            reporter,
+            configPath,
+        });
 
-    debug({
-        silent,
-        reporter,
-        configPath,
-    });
-
-    if (!silent) {
-        console.log('Stricter: Checking...');
-    }
-
-    debug('Read config');
-    const config = getConfig(configPath);
-
-    debug('Get file list');
-    const fileList = listFiles(config.root);
-
-    debug('Get rule definitions');
-    const ruleDefinitions = getRuleDefinitions(config);
-
-    debug('Get rule applications');
-    const ruleApplications = getRuleApplications(config, ruleDefinitions);
-
-    debug('Get files to process');
-    const filesToProcess = filterFilesToProcess(config.root, fileList, ruleApplications);
-
-    debug('Read files data');
-    const filesData = readFilesData(filesToProcess, [config.root], config.extensions);
-
-    debug('Apply rules');
-    const projectResult = applyProjectRules(config.root, filesData, ruleApplications);
-
-    debug('Massage logs');
-    const logs = compactProjectLogs(projectResult);
-
-    debug('Write logs');
-    if (reporter === Reporter.MOCHA) {
-        mochaLogger(logs);
-    } else {
-        consoleLogger(logs);
-    }
-
-    debug('Count errors');
-    const result = getErrorCount(logs);
-
-    if (!silent) {
-        if (result === 0) {
-            console.log('Stricter: No errors');
-        } else {
-            console.log(`Stricter: ${result} error${result > 1 ? 's' : ''}`);
+        if (!silent) {
+            console.log('Stricter: Checking...');
         }
-    }
 
-    if (debug.enabled) {
-        debug('Total time: %dms', Date.now() - now);
-    }
+        const config = measure('Read config', () => getConfig(configPath));
+
+        const fileList = measure('Get file list', () => listFiles(config.root));
+
+        const ruleDefinitions = measure('Get rule definitions', () => getRuleDefinitions(config));
+
+        const ruleApplications = measure('Get rule applications', () =>
+            getRuleApplications(config, ruleDefinitions),
+        );
+
+        const filesToProcess = measure('Get files to process', () =>
+            filterFilesToProcess(config.root, fileList, ruleApplications),
+        );
+
+        const filesData = measure('Read files data', () =>
+            readFilesData(filesToProcess, [config.root], config.extensions),
+        );
+
+        const projectResult = measure('Apply rules', () =>
+            applyProjectRules(config.root, filesData, ruleApplications),
+        );
+
+        const logs = measure('Massage logs', () => compactProjectLogs(projectResult));
+
+        measure('Write logs', () => {
+            if (reporter === Reporter.MOCHA) {
+                mochaLogger(logs);
+            } else {
+                consoleLogger(logs);
+            }
+        });
+
+        const result = measure('Count errors', () => getErrorCount(logs));
+
+        if (!silent) {
+            if (result === 0) {
+                console.log('Stricter: No errors');
+            } else {
+                console.log(`Stricter: ${result} error${result > 1 ? 's' : ''}`);
+            }
+        }
+
+        return result;
+    });
 
     return result;
 };
