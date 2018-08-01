7a37af4cbf907af979bf7df4578566a7647daccc
Alexey Shpakov <li0liq@gmail.com>
2018-08-01 20:51:27 +1000

    Fix TypeScript violations

--- src/dependencies/parse-imports.ts
+++ src/dependencies/parse-imports.ts
@@ -20,32 +20,37 @@ export default (ast: NodeTypes): ParsedImportsResult => {
     simple(
         ast,
         {
-            ImportDeclaration(node: ImportDeclaration, state: ParsedImportsResult) {
-                state.staticImports.push(node.source.value);
+            ImportDeclaration(node: NodeTypes, state: ParsedImportsResult) {
+                const casted = <ImportDeclaration>node;
+                state.staticImports.push(casted.source.value);
             },
-            ExportNamedDeclaration(node: ExportNamedDeclaration, state: ParsedImportsResult) {
-                if (node.source) {
-                    state.staticImports.push(node.source.value);
+            ExportNamedDeclaration(node: NodeTypes, state: ParsedImportsResult) {
+                const casted = <ExportNamedDeclaration>node;
+                if (casted.source) {
+                    state.staticImports.push(casted.source.value);
                 }
             },
-            ExportAllDeclaration(node: ExportAllDeclaration, state: ParsedImportsResult) {
-                if (node.source) {
-                    state.staticImports.push(node.source.value);
+            ExportAllDeclaration(node: NodeTypes, state: ParsedImportsResult) {
+                const casted = <string>node;
+                if (casted.source) {
+                    state.staticImports.push(casted.source.value);
                 }
             },
-            CallExpression(node: CallExpression, state: ParsedImportsResult) {
-                const callee: Node | Identifier = node.callee;
+            CallExpression(node: NodeTypes, state: ParsedImportsResult) {
+                const casted = <CallExpression>node;
+
+                const callee: Node | Identifier = casted.callee;
 
                 if (
                     callee &&
                     (callee.type === 'Import' ||
                         (callee.type === 'Identifier' &&
                             (callee as Identifier).name === 'require')) &&
-                    node.arguments &&
-                    node.arguments.length > 0 &&
-                    node.arguments[0].type === 'StringLiteral'
+                    casted.arguments &&
+                    casted.arguments.length > 0 &&
+                    casted.arguments[0].type === 'StringLiteral'
                 ) {
-                    state.dynamicImports.push((node.arguments[0] as StringLiteral).value);
+                    state.dynamicImports.push((casted.arguments[0] as StringLiteral).value);
                 }
             },
         },
