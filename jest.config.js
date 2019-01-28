135b3746f43a1c33d723d1f5a988278e4443ea36
Alexey Shpakov <li0liq@gmail.com>
2019-01-28 17:20:14 +1100

    No need to specify ts for jest, trying to fix yarn for Travis CI

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    "testRegex": "(\\.|/)test\\.ts$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json"
-    ],
     "testEnvironment": "node",
     "collectCoverage": true,
     "collectCoverageFrom" : ["src/**/*.ts", "!src/types/**", "!src/*.ts", "!src/**/test.ts"],
