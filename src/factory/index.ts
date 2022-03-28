d24dbb28f0c3dc909f914bdb78015ef36b530cac
Tim Keir <tkeir@atlassian.com>
2022-03-28 16:11:53 +1100

    Fix boolean import to allow enabling debug logger

--- src/factory/index.ts
+++ src/factory/index.ts
@@ -1,4 +1,4 @@
-import * as isCi from 'is-ci';
+import isCi from 'is-ci';
 import { stricter } from '../stricter';
 import { logger as getDebugLogger } from '../logger/get-debug-logger';
 import { logger as getNullLogger } from '../logger/get-null-logger';
